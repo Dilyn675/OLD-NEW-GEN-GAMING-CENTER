d4de3a8fafb8fa096105f1c571cc3ccc8f62313b
Dilyn675 <111885103+Dilyn675@users.noreply.github.com>
2024-03-26 19:45:45 -0500

    Update register.php

--- register.php
+++ register.php
@@ -9,9 +9,20 @@
     <p>Please fill in the form below to register:</p>
     <form action="insert.php" method="POST">
         <label for="name">Name:</label>
-        <input type="text" id="name" name="customerName" required><br><br>
-        <label for="email">Email:</label>
-        <input type="email" id="email" name="customerEmail" required><br><br>
+        <input type="text" id="name" name="Name" required><br><br>
+        <label for="UserPassword">UserPassword:</label>
+        <input type="text" id="userPassword" name="UserPassword" required><br><br>
+        <label for="Address">Address:</label>
+        <input type="text" id="address" name="Address" required><br><br>
+        <label for="PhoneNumber">PhoneNumber:</label>
+        <input type="text" id="phoneNumber" name="PhoneNumber" required><br><br>
+        <label for="ShippingAddress">ShippingAddress:</label>
+        <input type="text" id="shippingAddress" name="ShippingAddress" required><br><br>
+        <label for="CreditCardNumber">CreditCardNumber:</label>
+        <input type="text" id="creditCardNumber" name="CreditCardNumber" required><br><br>
+        <label for="CreditCardExpirationDate">CreditCardExpirationDate:</label>
+        <input type="date" id="creditCardExpirationDate" name="CreditCardExpirationDate" required><br><br>
+        <br>
         <input type="submit" value="Submit">
     </form>
 </body>
